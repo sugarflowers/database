ce99ec40d7ec82b61ceedcf5b2b46f29172e35e1
sugarflowers <nmtr1p@outlook.jp>
2023-12-18 13:54:30 +0900

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -49,9 +49,11 @@ pub fn open(dbpath: &str) -> Result<Connection, rusqlite::Error> {
     Ok(Connection::open(&dbpath)?)
 }
 
+/*
 pub fn open_in_memory() -> Result<Connection, rusqlite::Error> {
     Ok(Connection::open_in_memory()?)
 }
+*/
 
 /*
 use database::{open, Database};
